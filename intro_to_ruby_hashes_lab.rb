e468765848fd512b055bf6dfefa3860a59fffb40
Seth Maisel <seth.maisel@gmail.com>
2019-10-01 05:19:19 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,7 +8,7 @@ end
 
 def monopoly_with_third_tier
   monopoly = {:railroads => {:rent_in_dollars => 
-  {:rent1 => 7, :rent2 => 5, :rent3 => 10, :rent4 => 15}}}
+  {:rent1 => 7, :rent2 => 5, :rent3 => 10, :namet => {}}}}}
 end
 
 def monopoly_with_fourth_tier
